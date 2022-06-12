029a89e7f6bf48ab587d188cc3861a03ef0c5907
Favour Musenga <favourmusenga@gmail.com>
2022-06-12 13:16:45 +0200

    bugfix

--- issueTracker.backend/src/main/java/com/favourmusenga/issuetracker/security/SecurityConfig.java
+++ issueTracker.backend/src/main/java/com/favourmusenga/issuetracker/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.cors();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeHttpRequests()
                 .antMatchers(HttpMethod.POST,"/api/location").permitAll()
